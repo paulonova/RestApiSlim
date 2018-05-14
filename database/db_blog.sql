61fe7c7bc89fa70758fde586d12f1e5903dbee04
paulonova <paulonova144@gmail.com>
2018-05-14 21:10:55 +0200

    Database changing

--- database/db_blog.sql
+++ database/db_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 09, 2018 at 10:09 AM
+-- Generation Time: May 14, 2018 at 07:09 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.7
 
@@ -36,6 +36,13 @@ CREATE TABLE `comments` (
   `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`commentID`, `entryID`, `content`, `createdBy`, `createdAt`) VALUES
+(1, 1, 'Bra Jobbat!', 3, '2018-05-14 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -45,9 +52,17 @@ CREATE TABLE `comments` (
 CREATE TABLE `entries` (
   `entryID` int(11) NOT NULL,
   `title` varchar(1000) COLLATE utf8mb4_swedish_ci NOT NULL,
+  `content` varchar(1000) COLLATE utf8mb4_swedish_ci NOT NULL,
   `createdBy` int(11) NOT NULL,
   `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Dumping data for table `entries`
+--
+
+INSERT INTO `entries` (`entryID`, `title`, `content`, `createdBy`, `createdAt`) VALUES
+(1, 'My Birtth Party', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 3, '2018-05-14 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -61,6 +76,14 @@ CREATE TABLE `users` (
   `password` varchar(200) COLLATE utf8mb4_swedish_ci NOT NULL,
   `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `createdAt`) VALUES
+(3, 'paulonova', '$2y$10$tpU7Uqh.dFqt3xl7xKUHYuDBcnE10og.G4i9BftmyJ7yLFvVx0Mfe', '0000-00-00 00:00:00'),
+(8, 'pedro', '$2y$10$sXg82ySpSXp.3oModolFpOHCq2lb9GzEyJ3mDRl8WlUQyHZF7sE4u', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -92,17 +115,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `entries`
 --
 ALTER TABLE `entries`
-  MODIFY `entryID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `entryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
